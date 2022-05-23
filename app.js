459e2a746fab857b82e45af2cd7b265b047056a0
PieroJacinto <pierojacinto@gmail.com>
2022-05-22 22:43:03 -0300

    cambios de port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
   console.log("Estamos corriendo en el puerto " + PORT);
 });
